ab9cb5d3245875579348117af910ffb236cdb808
cesar zambrano <cesarzambranod@gmail.com>
2023-03-09 20:45:50 -0300

    correcion

--- src/app/cursos/components/detalle-curso/detalle-curso.component.ts
+++ src/app/cursos/components/detalle-curso/detalle-curso.component.ts
@@ -4,13 +4,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-detalle-curso',
   templateUrl: './detalle-curso.component.html',
-  styleUrls: ['./detalle-curso.component.scss']
+  styleUrls: ['./detalle-curso.component.css']
 })
 export class DetalleCursoComponent implements OnInit {
   constructor(
-    private route: ActivatedRoute 
+    private route: ActivatedRoute
   ){
-    
+
   }
 
   ngOnInit(){
